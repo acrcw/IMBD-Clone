5e6a4e7792ae8cbf10c5bf9e2253adba0bbbcdac
joban <joban9872@gmail.com>
2024-02-21 17:08:48 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={
+            element={ 
               <>
                 <Banner />
                 <Movies />
