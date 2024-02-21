6a8ea002b029ea536ec2c887a16b17830162df4a
joban <joban9872@gmail.com>
2024-02-21 16:40:50 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Movies from './components/Movies';
-import Login from './components/Login';
-import Banner from './components/Banner';
-import Navbar from './components/Navbar';
-import Newui from './components/Customui';
-import Favourites from './components/Favourites';
-import Pagination from './components/Pagination';
-import Signup from './components/Signup';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Movies from "./components/Movies";
+import Login from "./components/Login";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+import Newui from "./components/Customui";
+import Favourites from "./components/Favourites";
+import Pagination from "./components/Pagination";
+import Signup from "./components/Signup";
 function App() {
   return (
     <>
-    <BrowserRouter>
-
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<><Banner/><Movies/></>}></Route>
-        <Route path='/favourites' element={<Favourites/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-      </Routes>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <Movies />
+              </>
+            }
+          ></Route>
+          <Route path="/favourites" element={<Favourites />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
       </BrowserRouter>
       {/* <h2>Pagination</h2> */}
     </>
-
   );
 }
 
